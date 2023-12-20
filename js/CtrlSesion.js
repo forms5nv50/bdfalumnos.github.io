e86cdde2ddce97b1d9970a639ae87ce0ad73dbf6
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2023-12-20 05:42:19 -0600

    Update CtrlSesion.js

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -49,12 +49,13 @@ async function
       const userId = usuario.email;
       // Asigna el rol de "Cliente" al usuario
       asignarRolAdmin(userId);
+    } else {
+      // Usuario aceptado.
+      // Obtén el ID o correo electrónico del usuario al que deseas asignar el rol
+      const userId = usuario.email;
+      // Asigna el rol de "Cliente" al usuario
+      asignarRolCliente(userId);
     }
-    // Usuario aceptado.
-    // Obtén el ID o correo electrónico del usuario al que deseas asignar el rol
-    const userId = usuario.email;
-    // Asigna el rol de "Cliente" al usuario
-    asignarRolCliente(userId);
     forma.email.value =
       usuario.email || "";
     forma.nombre.value =
