dda9f55463b3e193af80f8dafbdbe026dab70745
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2023-12-19 22:34:46 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyD8VX-8N8zF6Ht2dv3EkQTp71lUXeJqLRg",
+    authDomain: "bdalumnos-af320.firebaseapp.com",
+    projectId: "bdalumnos-af320",
+    storageBucket: "bdalumnos-af320.appspot.com",
+    messagingSenderId: "108152656674",
+    appId: "1:108152656674:web:1dff7b4d1febcbdba80b7b"
 });
